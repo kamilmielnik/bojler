920364db0a8ab1fcccf4d6a4f428bfa157bc3ea6
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-06-06 23:15:44 +0700

    Auto-detect project

--- boil.js
+++ boil.js
@@ -9,9 +9,13 @@
  * It will add Dropdown.scss import to /resources/stylesheets/style.scss
  */
 
+const fs = require("fs");
 const os = require("os");
 const path = require("path");
 const { boil } = require("./Boiler");
+
+const BOIL_DIRECTORY = path.resolve(os.homedir(), ".boil");
+const DEFAULT_PROJECT = "prezly";
 
 const argv = Array.from(process.argv);
 const args = argv.slice(2);
@@ -21,12 +25,16 @@ if (typeof argRootPath !== "string") {
   throw new Error("Usage: node boil.js [path]");
 }
 
-const BOIL_DIRECTORY = path.resolve(os.homedir(), ".boil");
-
-const configFilepath = path.join(BOIL_DIRECTORY, "prezly.js");
-const config = require(configFilepath);
 const rootPath = path.isAbsolute(argRootPath)
   ? argRootPath
   : path.join(process.cwd(), argRootPath);
+const configFilenames = fs.readdirSync(BOIL_DIRECTORY);
+const configs = configFilenames.map((configFilename) => {
+  const configFilepath = path.join(BOIL_DIRECTORY, configFilename);
+  return require(configFilepath);
+});
+const config = configs.find((project) => {
+  return rootPath.includes(project.PROJECT_PATH);
+});
 
 boil(rootPath, config);
